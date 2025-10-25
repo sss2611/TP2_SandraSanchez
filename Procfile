623ad259f5248c0ea254ce9c901ada1dddf4ce24
sss2611 <solss2611@gmail.com>
2025-10-25 20:27:51 -0300

    mas app

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:application
+web: gunicorn app.wsgi:application
--- app/wsgi.py
+++ app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PT2_SandraSanchez.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
