842918b89ea4ef43f4f93a23e0122790a2710628
sss2611 <solss2611@gmail.com>
2025-10-25 20:26:42 -0300

    app

--- app/settings.py
+++ app/settings.py
@@ -30,8 +30,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'urls'
-WSGI_APPLICATION = 'PT2_SandraSanchez.app.wsgi.application'
+ROOT_URLCONF = 'app.urls'
+WSGI_APPLICATION = 'app.wsgi.application'
 
 TEMPLATES = [
     {
